d96b5d974b49538b1f2cd62734ff7299f080d7cc
Artur <artur.p.bebenek@gmail.com>
2021-05-13 17:31:42 +0100

    Finished adding all views in the planned MVP.

---
+++ WMSapp_BlazorServer/Models/NewExperimentComponentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMSapp_BlazorServer.Models
+{
+    public class NewExperimentComponentModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The Title is too long.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The Requirement is too long.")]
+        public string Requirement { get; set; }
+
+        [StringLength(1000, ErrorMessage = "The Description is too long.")]
+        public string Description { get; set; }
+    }
+}
---
+++ WMSapp_BlazorServer/Models/NewVehicleComponentModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WMSapp_BlazorServer.Models
+{
+    public class NewVehicleComponentModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200, ErrorMessage = "The Title is too long.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "The Vehicle Number is too long")]
+        public string VehicleNumber { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "The Build Number is too long")]
+        public string BuildNumber { get; set; }
+
+        public string Stage { get; set; }
+    }
+}
