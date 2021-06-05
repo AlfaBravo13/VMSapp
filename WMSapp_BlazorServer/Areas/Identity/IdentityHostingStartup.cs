900cca14d5fd16260439aaf847598a9db24729e3
Artur <artur.p.bebenek@gmail.com>
2021-06-05 14:57:28 +0100

    Authorization added.

---
+++ WMSapp_BlazorServer/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WMSapp_BlazorServer.Data;
+
+[assembly: HostingStartup(typeof(WMSapp_BlazorServer.Areas.Identity.IdentityHostingStartup))]
+namespace WMSapp_BlazorServer.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
